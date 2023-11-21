e57a4307b0839e22c32c4dc6df7daa50c0a6fbd7
Silio Leonardo (CtP/ETS) <sii5ct@bosch.com>
2023-11-21 10:38:45 -0300

    Init new propose implementation

--- src/State.cs
+++ src/State.cs
@@ -4,7 +4,8 @@ namespace Blindness;
 
 public class State
 {
-    private object obj;
-    private string field;
-    private Type type;
+    public Stateness Parent { get; set; }
+    public string Name { get; set; }
+    public bool IsProperty { get; set; }
+    public Type Type { get; set; }
 }
--- src/Stateness.cs
+++ src/Stateness.cs
@@ -7,4 +7,38 @@ namespace Blindness;
 public abstract class Stateness
 {
     private List<State> states = new();
+    private List<Stateness> children = new();
+
+    public Stateness(params object[] deps)
+    {
+
+    }
+
+    // TODOs
+    // -Identify recived fields has a parent state
+    // -Try remove unused states
+    private void loadStates()
+    {
+        var type = this.GetType();
+
+        foreach (var prop in type.GetRuntimeProperties())
+        {
+            this.states.Add(new() {
+                Parent = this,
+                Name = prop.Name,
+                IsProperty = true, 
+                Type = prop.PropertyType
+            });
+        }
+
+        foreach (var field in type.GetRuntimeFields())
+        {
+            this.states.Add(new() {
+                Parent = this,
+                Name = field.Name,
+                IsProperty = false, 
+                Type = field.FieldType
+            });
+        }
+    }
 }
