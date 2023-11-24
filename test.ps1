c1ed26c5a7fc0258c7b80ebe13cc7408f0734924
Trevisharp <sii5ct@bosch.com>
2023-11-24 13:51:33 -0300

    Update test.ps1 to generate updates to /buildTransitive/tools Code Generator based in /gen content

--- test.ps1
+++ test.ps1
@@ -1,5 +1,14 @@
+cd gen
+dotnet build -c Release
+cd ..
+
+cp .\gen\bin\Release\net7.0\CodeGenerator.exe .\src\buildTransitive\tools\CodeGenerator.exe
+cp .\gen\bin\Release\net7.0\CodeGenerator.dll .\src\buildTransitive\tools\CodeGenerator.dll
+cp .\gen\bin\Release\net7.0\CodeGenerator.runtimeconfig.json .\src\buildTransitive\tools\CodeGenerator.runtimeconfig.json
+
+
 cd src
-dotnet pack -o output
+dotnet pack -c Release -o output
 cd ..
 
 cd smp
