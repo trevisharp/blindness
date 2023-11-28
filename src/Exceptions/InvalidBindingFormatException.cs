2ffeb5610e1801f5bb7010791045e3680f06253f
trevisharp <leonardosilio@alunos.utfpr.edu.br>
2023-11-28 14:16:31 -0300

    Add Bind code to function

---
+++ src/Exceptions/InvalidBindingFormatException.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Blindness.Exceptions;
+
+public class InvalidBindingFormatException : Exception
+{
+    public override string Message =>
+        """
+            A Binding function need be in the format x => y.
+        """;
+}
--- src/Node.cs
+++ src/Node.cs
@@ -5,8 +5,56 @@ using System.Linq.Expressions;
 
 namespace Blindness;
 
+using Exceptions;
+
 public abstract class Node
 {
+    public void Bind(params Expression<Func<object, object>>[] bindings)
+    {
+        foreach (var binding in bindings)
+        {
+            var info = getBindingInformation(binding);
+            
+            // TODO
+        }
+    }
+
+    private (MemberInfo member, object obj) getBindingInformation(
+        Expression<Func<object, object>> binding
+    )
+    {
+        if (binding.Parameters.Count != 1)
+            throw new InvalidBindingFormatException();
+        var reciver = binding.Parameters[0].Name;
+        
+        var body = binding.Body;
+        var member = fieldObjectSearch(body);
+        if (member != null)
+            return (member, null);
+        
+        var func = binding.Compile();
+        return (null, func(null));
+    }
+
+    private MemberInfo fieldObjectSearch(Expression body)
+    {
+        switch (body.NodeType)
+        {
+            case ExpressionType.Convert:
+                var unaryExp = body as UnaryExpression;
+                var operand = unaryExp.Operand;
+                return fieldObjectSearch(operand);
+            
+            case ExpressionType.MemberAccess:
+                var memberExp = body as MemberExpression;
+                return memberExp.Member;
+            
+            case ExpressionType.ListInit:
+                return null;
+        }
+        
+        throw new InvalidBindingFormatException();
+    }
 
     public virtual void Load() { }
 }
@@ -16,16 +64,6 @@ public abstract class Node<T> : Node
 {
     public static T Get()
         => DependencySystem.Current.GetConcrete<T>();
-    public void Bind(params Expression<Func<T, object>>[] bindings)
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        foreach (var binding in bindings)
-        {
-            
-        }
-        Console.ResetColor();
-        throw new NotImplementedException();
-    }
 
     // TODOs
     // -Identify recived fields has a parent state
