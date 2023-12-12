bd7bf2f92082821965c9648dc2de7da7d60c97f3
trevisharp <leonardosilio@alunos.utfpr.edu.br>
2023-12-12 11:00:46 -0300

    Added Tree abstraction

---
+++ src/Tree.cs
@@ -0,0 +1,8 @@
+namespace Blindness;
+
+public class Tree
+{
+    public INode Root { get; set; }
+    public TreeMode Mode { get; set; }
+    
+}
---
+++ src/TreeMode.cs
@@ -0,0 +1,7 @@
+namespace Blindness;
+
+public enum TreeMode
+{
+    OnFrame,
+    OnTrigger
+}
