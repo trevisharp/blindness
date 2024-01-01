d1ad55d77b07c45015d5ce5d68d06b520fb8db9c
Trevisan <leonardosilio@alunos.utfpr.edu.br>
2024-01-01 19:20:58 -0300

    remove gen operations from project

--- test.ps1
+++ test.ps1
@@ -2,14 +2,6 @@ clear
 
 dotnet nuget delete Blindness 1.0.0 -s ($env:USERPROFILE + "\.nuget\packages") --non-interactive
 
-cd gen
-dotnet build -c Release
-cd ..
-
-cp .\gen\bin\Release\net7.0\CodeGenerator.exe .\src\buildTransitive\tools\CodeGenerator.exe
-cp .\gen\bin\Release\net7.0\CodeGenerator.dll .\src\buildTransitive\tools\CodeGenerator.dll
-cp .\gen\bin\Release\net7.0\CodeGenerator.runtimeconfig.json .\src\buildTransitive\tools\CodeGenerator.runtimeconfig.json
-
 cd src
 dotnet pack -c Release -o output
 cd ..
